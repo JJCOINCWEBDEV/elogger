84eebe42f007de5ac1ea903a9fa4b6a0ccb9a78d
Stanislav Lapata <stanislavlapata@gmail.com>
2020-01-13 19:27:06 +0300

    update dependences

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ELogger.MixProject do
   def project do
     [
       app: :elogger,
-      version: "1.0.0",
+      version: "1.1.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps()
@@ -22,7 +22,7 @@ defmodule ELogger.MixProject do
   defp deps do
     [
       {:jason, "~> 1.1"},
-      {:plug, "~> 1.8"}.
+      {:plug, "~> 1.8"},
       {:sentry, "~> 7.2"}
     ]
   end
